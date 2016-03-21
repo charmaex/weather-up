eb2cf91d971c88f29c8fd1e9b313de0fc3babc40
Jan Dammshäuser <jandammshaeuser@me.com>
2016-03-21 09:34:42 +0100

    Tweaked Styles variable naming

--- WeatherUp/Styles.swift
+++ WeatherUp/Styles.swift
@@ -9,72 +9,76 @@
 import Foundation
 import UIKit
 
-extension UILabel {
-    func applyStyle(style: FontStyles) {
-        let (font, color) = style.getStyle()
-        
-        self.font = font
-        self.textColor = color
-    }
-}
-
-class ColorPalette: UIColor {
+class ColorPalette {
     
     class func textColor() -> UIColor {
         return rgba(108, 244, 255, 1)
     }
     
-    private class func rgba(r: CGFloat, _ g: CGFloat, _ b: CGFloat, _ a: CGFloat) -> UIColor {
-        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: a)
-    }
 }
+
 
 enum Fonts: String {
     
     case GillSans
     
-    enum FontType: String {
+    enum Weights: String {
         case Regular = ""
         case Italic = "-Italic"
         case Light = "-Light"
     }
     
-    func name(type: FontType) -> String {
-        return "\(self.rawValue)\(type.rawValue)"
-    }
-    
 }
 
+    
 enum FontStyles {
     
     case H1, H2, H3, Paragraph, H6, TextStyle
     
-    func getStyle() -> (UIFont, UIColor) {
-        return (font(), color())
-    }
-    
-    private func color() -> UIColor {
+    func color() -> UIColor {
         switch self {
         default:
             return ColorPalette.textColor()
         }
     }
     
-    private func font() -> UIFont {
+    func font() -> UIFont {
         switch self {
         case .H1:
-            return UIFont(name: Fonts.GillSans.name(.Regular), size: 12)!
+            return UIFont(name: Fonts.GillSans.name(weight: .Regular), size: 12)!
         case .H2:
-            return UIFont(name: Fonts.GillSans.name(.Italic), size: 12)!
+            return UIFont(name: Fonts.GillSans.name(weight: .Italic), size: 12)!
         case .H3:
-            return UIFont(name: Fonts.GillSans.name(.Light), size: 12)!
+            return UIFont(name: Fonts.GillSans.name(weight: .Light), size: 12)!
         case .Paragraph:
-            return UIFont(name: Fonts.GillSans.name(.Regular), size: 12)!
+            return UIFont(name: Fonts.GillSans.name(weight: .Regular), size: 12)!
         case .H6:
-            return UIFont(name: Fonts.GillSans.name(.Regular), size: 12)!
+            return UIFont(name: Fonts.GillSans.name(weight: .Regular), size: 12)!
         case .TextStyle:
-            return UIFont(name: Fonts.GillSans.name(.Regular), size: 12)!
+            return UIFont(name: Fonts.GillSans.name(weight: .Regular), size: 12)!
         }
     }
     
+}
+
+
+// supporting extensions
+
+extension UILabel {
+    func applyStyle(style: FontStyles) {
+        self.font = style.font()
+        self.textColor = style.color()
+    }
+}
+
+extension ColorPalette {
+    private class func rgba(r: CGFloat, _ g: CGFloat, _ b: CGFloat, _ a: CGFloat) -> UIColor {
+        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: a)
+    }
+}
+
+extension Fonts {
+    func name(weight weight: Weights) -> String {
+        return "\(self.rawValue)\(weight.rawValue)"
+    }
 }
