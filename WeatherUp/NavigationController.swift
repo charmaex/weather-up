c9c83e0674b596757810d2c7e0387d44e758e74d
Jan Damm <jandammshaeuser@me.com>
2016-10-05 21:39:15 +0200

    Updated swiftformat; Added file

---
+++ WeatherUp/NavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationController.swift
+//  WeatherUp
+//
+//  Created by Jan Dammshäuser on 05.10.16.
+//  Copyright © 2016 Jan Dammshäuser. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
